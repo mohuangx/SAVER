258fa41647b61afae26da7d7e450afe6855db638
Mo Huang <mohuang@wharton.upenn.edu>
2017-05-29 16:08:10 -0400

    Documenting dataset and package.

---
+++ R/SAVER-package.R
@@ -0,0 +1,14 @@
+#' SAVER: Single-cell Analysis Via Expression Recovery
+#'
+#' The SAVER package implements SAVER, a gene expression recovery method for
+#' single-cell RNA sequencing (scRNA-seq). Borrowing information across all
+#' genes and cells, SAVER provides estimates for true expression levels as well
+#' as posterior distributions to account for estimation uncertainty.
+#'
+#' @author Mo Huang, \url{mohuang@wharton.upenn.edu}
+#'
+#' @source \url{https://github.com/mohuangx/SAVER}
+#'
+#' @docType package
+#' @name SAVER-package
+NULL
---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Mouse brain single-cell RNA-seq dataset
+#'
+#' 3,529 genes and 500 cells from a mouse brain scRNA-seq dataset.
+#'
+#' @references Zeisel, A., Munoz-Manchado, A. B., Codeluppi, S.,
+#' Lonnerberg, P., La Manno, G., Jureus, A., ... Linnarsson, S. (2015). Cell
+#' types in the mouse cortex and hippocampus revealed by single-cell RNA-seq.
+#' \emph{Science, 347}(6226), 1138-1142.
+#'
+"linnarsson"
