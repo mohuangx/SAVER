55aaee1ddcc48b1a82a7fcafd77948b0bdbaebf6
Mo Huang <mohuang@wharton.upenn.edu>
2018-02-27 21:17:03 -0500

    Include lambda.coefs

--- R/combine_saver.R
+++ R/combine_saver.R
@@ -22,15 +22,16 @@
 combine.saver <- function(saver.list) {
   est <- do.call(rbind, lapply(saver.list, `[[`, 1))
   se <- do.call(rbind, lapply(saver.list, `[[`, 2))
-  info <- vector("list", 9)
+  info <- vector("list", 10)
   names(info) <- c("size.factor", "maxcor", "lambda.max", "lambda.min",
-                   "sd.cv", "pred.time", "var.time", "cutoff", "total.time")
+                   "sd.cv", "pred.time", "var.time", "cutoff", 
+                   "lambda.coefs", "total.time")
   info[[1]] <- saver.list[[1]]$info$size.factor
   info.list <- lapply(saver.list, `[[`, 3)
-  for (i in 2:8) {
+  for (i in 2:9) {
     info[[i]] <- do.call(c, lapply(info.list, `[[`, i))
   }
-  info[[9]] <- do.call(sum, (lapply(info.list, `[[`, 9)))
+  info[[10]] <- do.call(sum, (lapply(info.list, `[[`, 9)))
   out <- list(estimate = est, se = se, info = info)
   class(out) <- "saver"
   out
