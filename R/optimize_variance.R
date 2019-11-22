8158d129e650a440e35fc1dea1b178424d8ba46d
Quanli Wang <quanliwang20@hotmail.com>
2019-11-22 09:36:37 -0500

    Consolidate calc_a, calc_b and calc_k into one function for now.

--- R/optimize_variance.R
+++ R/optimize_variance.R
@@ -19,6 +19,7 @@
 #'
 #'
 #' @importFrom  stats optimize ppoints uniroot var
+#'
 #' @rdname optimize_variance
 #' @export
 calc.a <- function(y, mu, sf) {
@@ -26,18 +27,18 @@ calc.a <- function(y, mu, sf) {
   if (sum(mu) == 0) {
     return(c(0, 0))
   }
-  a.vec <- optimize(calc_loglik_a, interval = c(0, var(y/sf)/mean(y/sf)^2), y = y, mu = mu, sf = sf)
+  interval_max <- var(y/sf)/mean(y/sf)^2
+  a.vec <- optimize(calc_loglik_a, interval = c(0, interval_max), y = y, mu = mu, sf = sf)
   a <- a.vec$minimum
   a.loglik <- a.vec$objective
   min.a <- -calc_loglik_a(1e-05, y, mu, sf)
   mle.a <- -calc_loglik_a(a, y, mu, sf)
-  max.a <- -calc_loglik_a(var(y/sf)/mean(y/sf)^2, y, mu, sf)
   if (mle.a - min.a < 0.5) {
+    max.a <- -calc_loglik_a(interval_max, y, mu, sf)
     if (mle.a-max.a > 10) {
-      a.max <- uniroot(function(x) calc_loglik_a(x, y, mu, sf) + mle.a - 10,
-                       c(1e-05, var(y/sf)/mean(y/sf)^2))$root
+      a.max <- uniroot(function(x) calc_loglik_a(x, y, mu, sf) + mle.a - 10,  c(1e-05, interval_max))$root
     } else {
-      a.max <- var(y/sf)/mean(y/sf)^2
+      a.max <- interval_max
     }
     samp <- (exp((exp(ppoints(100))-1)/2)-1)*a.max
     loglik <- calc_loglik_a(samp, y, mu, sf)
@@ -56,20 +57,18 @@ calc.b <- function(y, mu, sf) {
   if (sum(mu) == 0) {
     return(c(0, 0))
   }
-  var_over_mean <- var(y/sf)/mean(y/sf)
-  b.vec <- optimize(calc_loglik_b, interval = c(0, var_over_mean), y = y, mu = mu, sf = sf)
+  interval_max <- var(y/sf)/mean(y/sf)
+  b.vec <- optimize(calc_loglik_b, interval = c(0, interval_max), y = y, mu = mu, sf = sf)
   b <- b.vec$minimum
   b.loglik <- b.vec$objective
-  
   min.b <- -calc_loglik_b(1e-05, y, mu, sf)
   mle.b <- -calc_loglik_b(b, y, mu, sf)
- 
   if (mle.b - min.b < 0.5) {
-    max.b <- -calc_loglik_b(var_over_mean, y, mu, sf)
+    max.b <- -calc_loglik_b(interval_max, y, mu, sf)
     if (mle.b-max.b > 10) {
-      b.max <- uniroot(function(x) calc_loglik_b(x, y, mu, sf) + mle.b - 10, c(1e-05, var_over_mean))$root
+      b.max <- uniroot(function(x) calc_loglik_b(x, y, mu, sf) + mle.b - 10, c(1e-05, interval_max))$root
     } else {
-      b.max <- var_over_mean
+      b.max <- interval_max
     }
     samp <- (exp((exp(ppoints(100))-1)/2)-1)*b.max
     loglik <- calc_loglik_b(samp, y, mu, sf)
@@ -88,17 +87,18 @@ calc.k <- function(y, mu, sf) {
   if (sum(mu) == 0) {
     return(c(0, 0))
   }
-  k.vec <- optimize(calc_loglik_k, interval = c(0, var(y/sf)), y = y, mu = mu,sf = sf)
+  interval_max <- var(y/sf)
+  k.vec <- optimize(calc_loglik_k, interval = c(0, interval_max), y = y, mu = mu,sf = sf)
   k <- k.vec$minimum
   k.loglik <- k.vec$objective
   min.k <- -calc_loglik_k(1e-05, y, mu, sf)
   mle.k <- -calc_loglik_k(k, y, mu, sf)
-  max.k <- -calc_loglik_k(var(y/sf), y, mu, sf)
   if (mle.k - min.k < 0.5) {
+    max.k <- -calc_loglik_k(interval_max, y, mu, sf)
     if (mle.k-max.k > 10) {
-      k.max <- uniroot(function(x) calc_loglik_k(x, y, mu, sf) + mle.k - 10, c(1e-05, var(y/sf)))$root
+      k.max <- uniroot(function(x) calc_loglik_k(x, y, mu, sf) + mle.k - 10, c(1e-05, interval_max))$root
     } else {
-      k.max <- var(y/sf)
+      k.max <- interval_max
     }
     samp <- (exp((exp(ppoints(100))-1)/2)-1)*k.max
     loglik <- calc_loglik_k(samp, y, mu, sf)
@@ -106,6 +106,46 @@ calc.k <- function(y, mu, sf) {
     loglik3 <- loglik2/sum(loglik2)
     k <- sum(samp * loglik3)
     k.loglik <- calc_loglik_k(k, y, mu, sf)
+  }
+  return(c(k, k.loglik))
+}
+
+#' @rdname optimize_variance
+#' @export
+calc.abk <- function(y, mu, sf, type) {
+  n <- length(y)
+  if (sum(mu) == 0) {
+    return(c(0, 0))
+  }
+  if (type == "a") {
+      interval_max <- var(y/sf)/mean(y/sf)^2
+      f <- calc_loglik_a
+  } else if (type == "b") {
+      interval_max <- var(y/sf)/mean(y/sf)
+      f <- calc_loglik_b
+  } else {
+      interval_max <- var(y/sf)
+      f <- calc_loglik_k
+  }
+  interval_max <- var(y/sf)
+  k.vec <- optimize(f, interval = c(0, interval_max), y = y, mu = mu,sf = sf)
+  k <- k.vec$minimum
+  k.loglik <- k.vec$objective
+  min.k <- -f(1e-05, y, mu, sf)
+  mle.k <- -f(k, y, mu, sf)
+  if (mle.k - min.k < 0.5) {
+    max.k <- -f(interval_max, y, mu, sf)
+    if (mle.k-max.k > 10) {
+      k.max <- uniroot(function(x) f(x, y, mu, sf) + mle.k - 10, c(1e-05, interval_max))$root
+    } else {
+      k.max <- interval_max
+    }
+    samp <- (exp((exp(ppoints(100))-1)/2)-1)*k.max
+    loglik <- f(samp, y, mu, sf)
+    loglik2 <- exp(-loglik-min(-loglik))
+    loglik3 <- loglik2/sum(loglik2)
+    k <- sum(samp * loglik3)
+    k.loglik <- f(k, y, mu, sf)
   }
   return(c(k, k.loglik))
 }
